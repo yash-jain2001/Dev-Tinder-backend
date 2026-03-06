60ec11d6e701a3d062a7d9f4d832d9870343a66a
Priyanshu jain <89932280+yash-jain2001@users.noreply.github.com>
2026-03-06 18:56:08 +0530

    checking auth using middlewares

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ const express = require("express");
 const app = express();
 
 app.use(
-  "/user",
+  "/admin",
   (req, res, next) => {
-    console.log("response 1");
-    res.send("response 1 ");
-    next();
-  },
-
-  (req, res) => {
-    console.log("response 2");
-    res.send("response 2 ");
-  },
+    const token = "abc"
+    const isAuthorized = token==='abc'
+    if(isAuthorized){
+      next();
+    }else{
+      res.status(403).send("Unauthorized");
+    }  
+  }
 );
+
+app.get("/admin/getuser",(req,res)=>{
+  res.send("get user data")
+})
+app.get("/admin/deleteuser",(req,res)=>{
+  res.send("delete user data")
+})
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
