c0e059d1feda28d4591424c63e3f0bfadc12a7a1
Priyanshu jain <89932280+yash-jain2001@users.noreply.github.com>
2026-03-07 14:41:39 +0530

    database connectivity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 const express = require("express");
+require("./config/database.js");
 const app = express();
-const adminAuth = require("./middlewares/auth");
-
-app.get("/admin/getuser",(req, res)=>{
-  throw new Error("Something went wrong");
-  res.send("get user data")
-})
-
-app.use("/",(err, req,res,next)=>{
-  if(err){
-    res.status(500).send("Internal Server Error")
-  }
-})
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
